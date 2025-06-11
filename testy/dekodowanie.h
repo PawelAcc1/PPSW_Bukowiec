cc9e09f344954947d0f8367a106f11ff50eca738
Paweł Bukowiec <pawelbukowiec66@gmail.com>
2025-06-11 10:40:18 +0200

    testy

---
+++ testy/dekodowanie.h
@@ -0,0 +1,36 @@
+#define MAX_TOKEN_NR 3
+#define MAX_KEYWORD_STRING_LTH 10
+#define DELIMITER_CHARACTER ' '
+
+enum KeywordCode {LD, ST, RST};
+enum TokenType { KEYWORD, NUMBER, STRING };
+
+
+union TokenValue {
+  enum KeywordCode eKeyword;
+  unsigned int uiValue;
+  char *pcString;
+};
+
+struct Token{
+	enum TokenType eType; 
+	union TokenValue uValue;
+};
+
+
+struct Keyword {
+	enum KeywordCode eCode;
+	char cString[MAX_KEYWORD_STRING_LTH + 1];
+};
+
+extern struct Token asToken[MAX_TOKEN_NR];
+extern unsigned char ucTokenNr;
+
+
+unsigned char ucFindTokensInString(char *pcString);
+
+enum Result eStringToKeyword(char pcStr[], enum KeywordCode *peKeywordCode);
+
+void DecodeTokens(void);
+
+void DecodeMsg(char *pcString);
---
+++ testy/stringi.c
@@ -0,0 +1,65 @@
+#include <LPC21xx.H>
+#include "stringi.h"
+
+#define NULL 0
+
+void CopyString(char pcSource[], char pcDestination[])
+{
+
+	unsigned char ucCharCounter;
+
+  for (ucCharCounter = 0 ; pcSource[ucCharCounter] != NULL ; ucCharCounter = ucCharCounter + 1)
+	{
+		
+		pcDestination[ucCharCounter] = pcSource[ucCharCounter];
+  }
+	
+	pcDestination[ucCharCounter] = '\0';
+}
+
+enum CompResult { DIFFERENT , EQUAL };
+
+enum CompResult eCompareString(char pcStr1[], char pcStr2[]){
+	
+	unsigned char ucCharCounter;
+
+	for (ucCharCounter = 0 ; pcStr1[ucCharCounter] != NULL || pcStr2[ucCharCounter] != NULL ; ucCharCounter = ucCharCounter + 1){
+
+		if (pcStr1[ucCharCounter] != pcStr2[ucCharCounter]){
+            
+			return DIFFERENT;
+    }
+  }
+
+    return EQUAL;
+}
+
+void AppendString (char cSourceStr[],char cDestinationStr[])
+{
+    
+	unsigned char ucCharCounter;
+
+  for (ucCharCounter = 0 ; cDestinationStr[ucCharCounter] != NULL ; ucCharCounter = ucCharCounter + 1)
+	{
+
+  }
+
+	CopyString(cSourceStr, cDestinationStr + ucCharCounter);
+
+}
+
+void ReplaceCharactersInString(char pcString[],char cOldChar,char cNewChar){
+
+	unsigned char ucCharCounter;
+
+	for (ucCharCounter = 0 ; pcString[ucCharCounter] != NULL ; ucCharCounter = ucCharCounter + 1)
+	{
+
+		if ( pcString[ucCharCounter] == cOldChar )
+		{
+
+			pcString[ucCharCounter] = cNewChar;
+    }
+  }
+
+}
